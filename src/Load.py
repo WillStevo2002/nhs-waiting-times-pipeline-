a88111ac62948e1b21f3d54367df369b9fdcde34
WillStevo2002 <153753948+WillStevo2002@users.noreply.github.com>
2026-06-17 20:10:36 +0100

    Update Load.py

--- src/Load.py
+++ src/Load.py
@@ -1,1 +1,10 @@
+import sqlite3
 
+def load(df):
+    conn = sqlite3.connect("database.db")
+    df.to_sql(
+        "waiting_times",
+        conn,
+        if_exists="replace",
+        index=False
+    )
