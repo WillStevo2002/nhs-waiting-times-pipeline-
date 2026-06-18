2f1fc598a241ced5ef5fbfa902ab5254f4d690f4
WillStevo2002 <153753948+WillStevo2002@users.noreply.github.com>
2026-06-18 19:18:39 +0100

    Update Create_VIews.sql

--- SQL/Create_VIews.sql
+++ SQL/Create_VIews.sql
@@ -1,1 +1,16 @@
+CREATE VIEW monthly_trend AS
 
+SELECT
+    reporting_month,
+    SUM(patients_waiting) AS total_waiting
+FROM waiting_times
+GROUP BY reporting_month;
+
+
+CREATE VIEW health_board_comparison AS
+
+SELECT
+    health_board,
+    SUM(patients_waiting) AS total_waiting
+FROM waiting_times
+GROUP BY health_board;
