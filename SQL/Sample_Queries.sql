89be76be474915ead44915642f81aa3bcc81c0de
WillStevo2002 <153753948+WillStevo2002@users.noreply.github.com>
2026-06-18 19:25:46 +0100

    Update Sample_Queries.sql

--- SQL/Sample_Queries.sql
+++ SQL/Sample_Queries.sql
@@ -1,1 +1,21 @@
+-- Patients Waiting by Health Board Sort
+SELECT
+    health_board,
+    SUM(patients_waiting)
+FROM waiting_times
+GROUP BY health_board;
 
+-- Waiting List per Speciality (Departments)
+SELECT
+    specialty,
+    AVG(patients_waiting)
+FROM waiting_times
+GROUP BY specialty;
+
+-- Breakdown of Patients waiting by Month
+SELECT
+    reporting_month,
+    patients_waiting,
+    LAG(patients_waiting)
+    OVER (ORDER BY reporting_month)
+FROM waiting_times;
