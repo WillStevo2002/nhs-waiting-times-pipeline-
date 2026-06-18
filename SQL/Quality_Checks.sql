689983cdf9b0b7e6f819da036b882fb8b0d1fc24
WillStevo2002 <153753948+WillStevo2002@users.noreply.github.com>
2026-06-18 19:19:13 +0100

    Update Quality_Checks.sql

--- SQL/Quality_Checks.sql
+++ SQL/Quality_Checks.sql
@@ -1,1 +1,17 @@
+-- Null values
 
+SELECT *
+FROM waiting_times
+WHERE patients_waiting IS NULL;
+
+-- Negative values
+
+SELECT *
+FROM waiting_times
+WHERE patients_waiting < 0;
+
+-- Missing Health Board
+
+SELECT *
+FROM waiting_times
+WHERE health_board IS NULL;
